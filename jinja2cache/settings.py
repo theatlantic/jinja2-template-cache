b70ad6daf416c03e10bba3ef284b811eb5be3c72
Frankie Dintino <fdintino@theatlantic.com>
2012-06-05 11:15:19 -0400

    Making JINJA2_CACHE_MSTAT_DISABLED default to False

--- jinja2cache/settings.py
+++ jinja2cache/settings.py
@@ -4,4 +4,4 @@ JINJA2_CACHE_BACKEND = getattr(settings, 'JINJA2_CACHE_BACKEND', None)
 JINJA2_CACHE_ENABLED = getattr(settings, 'JINJA2_CACHE_ENABLED', False)
 
 # Whether to check for updates with an mstat before loading a template from cache.
-JINJA2_CACHE_MSTAT_DISABLED = getattr(settings, 'JINJA2_CACHE_MSTAT_DISABLED', True)
+JINJA2_CACHE_MSTAT_DISABLED = getattr(settings, 'JINJA2_CACHE_MSTAT_DISABLED', False)
